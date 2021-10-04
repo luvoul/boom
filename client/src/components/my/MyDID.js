b4bd08164cade0e4060448be525a79aea8c90fd0
[luvoul] <85976276+luvoul@users.noreply.github.com>
2021-10-04 20:20:05 +0900

    mycredential

--- client/src/components/my/MyDID.js
+++ client/src/components/my/MyDID.js
@@ -1,11 +1,16 @@
 import React from "react";
-import HomeBar from "../home/HomeBar";
+import logo from "../../assets/img/boom.png";
+import { Link } from "react-router-dom";
 
 function MyDID() {
   return (
     <div>
-      <HomeBar />
-      <h1>나의 DID 정보 페이지입니다.</h1>
+      <div id="gotohome">
+        <Link to="/">
+          <img src={logo} alt="로고" width="160px" height="90px" />
+        </Link>
+        <h1>My DID</h1>
+      </div>
     </div>
   );
 }
